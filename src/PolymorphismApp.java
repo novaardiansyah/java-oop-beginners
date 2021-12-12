3958d9d6943db12ec2856ac7eb14f4a0583b4710
novaardiansyah <novaardiansyah78@gmail.com>
2021-12-12 11:21:04 +0700

    Type Check and Casts

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -2,16 +2,14 @@ public class PolymorphismApp {
   public static void main(String[] args) {
     System.out.println(""); // ? this is a new line
 
-    Employee employee = new Employee("Nova Ardiansyah", 23);
-    employee.sayHello();
+    // Employee employee = new Employee("Nova Ardiansyah", 23);
+    // employee.sayHello();
 
-    employee = new Manager("Dea Setiana Wati", 21);
-    employee.sayHello();
+    // employee = new Manager("Dea Setiana Wati", 21);
+    // employee.sayHello();
 
-    employee = new VicePresident("Aditiya Saputra", 14);
-    employee.sayHello();
-
-    System.out.println(""); // ? this is a new line
+    // employee = new VicePresident("Aditiya Saputra", 14);
+    // employee.sayHello();
 
     sayHello(new Employee("Nova Ardiansyah", 23));
     sayHello(new Manager("Dea Setiana Wati", 21));
@@ -19,6 +17,19 @@ public class PolymorphismApp {
   }
 
   static void sayHello(Employee employee) {
-    System.out.println("Hello " + employee.fullName);
+    if (employee instanceof VicePresident) {
+      // * casts the object to VicePresident
+      VicePresident vicePresident = (VicePresident) employee;
+      System.out.println(
+          "Hello, I'm VicePresident " + vicePresident.fullName + " and I'm " + vicePresident.age + " years old.");
+    } else if (employee instanceof Manager) {
+      // * casts the object to Manager
+      Manager manager = (Manager) employee;
+      System.out.println(
+          "Hello, I'm Manager " + manager.fullName + " and I'm " + manager.age + " years old.");
+    } else {
+      System.out.println(
+          "Hello, I'm Employee " + employee.fullName + " and I'm " + employee.age + " years old.");
+    }
   }
 }
