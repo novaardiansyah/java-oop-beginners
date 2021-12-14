3127c16f38663097fa6bbbb2375c8aa30ba454d4
novaardiansyah <novaardiansyah78@gmail.com>
2021-12-14 09:05:38 +0700

    static

--- src/app/StaticApp.java
+++ src/app/StaticApp.java
@@ -1,28 +1,29 @@
 package app;
 
-import app.data.Country;
-import app.data.Application;
-import app.data.Constant;
-import app.util.MathUtil;
+// ! static import
+import static app.data.Country.City;
+import static app.data.Constant.*;
+import static app.data.Application.*;
+import static app.util.MathUtil.*;
 
 public class StaticApp {
   public static void main(String[] args) {
     System.out.println(""); // ? newline
 
     // ? call or run static field
-    System.out.println("Application: " + Constant.APPLICATION);
-    System.out.println("Version: " + Constant.VERSION);
+    System.out.println("Application: " + APPLICATION);
+    System.out.println("Version: " + VERSION);
 
     // ? call or run static method
-    System.out.println("SUM: " + MathUtil.sum(23, 44, 55, 63, 12));
+    System.out.println("SUM: " + sum(23, 44, 55, 63, 12));
 
     // ? call or run static inner class
-    Country.City city = new Country.City();
+    City city = new City();
     city.setName("Lampung");
 
     System.out.println("City: " + city.getName());
 
     // ? call or run static block
-    System.out.println("Processors: " + Application.PROCESSORS);
+    System.out.println("Processors: " + PROCESSORS);
   }
 }
