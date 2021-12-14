018c47cfea0510b79051e8a9fdde9e5d80d05b5b
novaardiansyah <novaardiansyah78@gmail.com>
2021-12-14 16:19:27 +0700

    convert enum values into strings, objects and arrays

--- src/app/EnumApp.java
+++ src/app/EnumApp.java
@@ -14,5 +14,22 @@ public class EnumApp {
     System.out.println("Fullname: " + customer.getName());
     System.out.println("Level: " + customer.getLevel());
     System.out.println("Description: " + customer.getLevel().getDescription());
+
+    System.out.println(""); // ? newline
+
+    // ? convert to String
+    String levelName = Level.PREMIUM.name();
+    System.out.println(levelName);
+
+    // ? convert to Object
+    Level level = Level.valueOf("STANDART");
+    System.out.println(level);
+
+    // ? convert to Array
+    Level[] levels = Level.values();
+    System.out.println("\nArray Level:");
+    for (var value : levels) {
+      System.out.println(value);
+    }
   }
 }
